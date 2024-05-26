77642da2aa1de805c15b7d31d8ec9c162e62f68d
2zooo0 <c20221460@cau.ac.kr>
2024-05-27 00:41:05 +0900

    기본 홈페이지

---
+++ index.js
@@ -0,0 +1,32 @@
+//베이스URL가져오기
+const baseURL = "http://likelion12thtoy.kro.kr:8000/guestbook/";
+//자식요소 넣을 돔 가져오기(나누다보니,,,많아짐)
+const container = document.getElementById('container');
+const GuestList = document.getElementById('GuestList');
+const newGuest = document.getElementById('newGuest');
+const GList = document.getElementById('GList');
+const newG = document.getElementById('newG');
+
+//방명록 리스트 조회(시간순 정렬)
+async function getData() {
+    const url= `${baseURL}/`
+
+
+
+
+}
+
+
+
+//방명록 생성(작성)
+
+
+
+
+//방명록 조회
+
+
+
+
+
+//방명록 삭제
